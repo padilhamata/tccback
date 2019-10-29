31d6709a6034827bcaf56809fafd0c467052a492
Lucas <lucaspadilhamata@gmail.com>
2019-10-29 06:47:10 -0300

    Fazendo cors

--- src/main/java/com/pizza/backpizza/models/Produto.java
+++ src/main/java/com/pizza/backpizza/models/Produto.java
@@ -9,52 +9,57 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "TB_PRODUTO")
-public class Produto implements Serializable{
-
-	/**
-	 * 
-	 */
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
-@Id
-@GeneratedValue(strategy=GenerationType.AUTO)
-private long id;
-private String nome;
-private BigDecimal quantidade;
-private BigDecimal valor;
-public long getId() {
-	return id;
-}
-public void setId(long id) {
-	this.id = id;
-}
-public String getNome() {
-	return nome;
-}
-public void setNome(String nome) {
-	this.nome = nome;
-}
-public BigDecimal getQuantidade() {
-	return quantidade;
-}
-public void setQuantidade(BigDecimal quantidade) {
-	this.quantidade = quantidade;
-}
-public BigDecimal getValor() {
-	return valor;
-}
-public void setValor(BigDecimal valor) {
-	this.valor = valor;
-}
-public static long getSerialversionuid() {
-	return serialVersionUID;
-}
-@Override
-public String toString() {
-	return "Produto [id=" + id + ", nome=" + nome + ", quantidade=" + quantidade + ", valor=" + valor + "]";
-}
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String nome;
+	private BigDecimal quantidade;
+	private BigDecimal valor;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(BigDecimal quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nome=" + nome + ", quantidade=" + quantidade + ", valor=" + valor + "]";
+	}
 
 }
--- src/main/java/com/pizza/backpizza/resources/ProdutoResource.java
+++ src/main/java/com/pizza/backpizza/resources/ProdutoResource.java
@@ -3,6 +3,7 @@ package com.pizza.backpizza.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,36 +13,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pizza.backpizza.models.Produto;
 import com.pizza.backpizza.repository.ProdutoRepository;
 
-import com.pizza.backpizza.models.Produto;
-
 @RestController
-@RequestMapping(value="/api")
+@RequestMapping(value = "/api")
 public class ProdutoResource {
 
-		@Autowired
-		ProdutoRepository produtoRepository;
-		
-		@GetMapping("/produtos")
-		public  List<Produto> listaProdutos(){
-			return produtoRepository.findAll();	
-		}
-		@GetMapping("/produtos/{id}")
-		public  Produto listaProdutosUnico(@PathVariable(value="id") long id){
-			return produtoRepository.findById(id);	
-		}
-		@PostMapping("/produto")
-		public Produto salvaProduto(@RequestBody Produto produto) {
-			return produtoRepository.save(produto);
-		}
-		@DeleteMapping("/produto")
-		public void deletaProduto(@RequestBody Produto produto) {
-			produtoRepository.delete(produto);
-		}
-		@PutMapping("/produto")
-		public Produto atualizaProduto(@RequestBody Produto produto) {
-			System.out.println(produto);
-			return produtoRepository.save(produto);
-		}
+	@Autowired
+	ProdutoRepository produtoRepository;
+
+	@GetMapping("/produtos")
+	public List<Produto> listaProdutos() {
+		return produtoRepository.findAll();
+	}
+
+	@GetMapping("/produtos/{id}")
+	public Produto listaProdutosUnico(@PathVariable(value = "id") long id) {
+		return produtoRepository.findById(id);
+	}
+
+	@CrossOrigin
+	@PostMapping("/produto")
+	public Produto salvaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	@DeleteMapping("/produto")
+	public void deletaProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
+
+	@PutMapping("/produto")
+	public Produto atualizaProduto(@RequestBody Produto produto) {
+		System.out.println(produto);
+		return produtoRepository.save(produto);
+	}
 }
